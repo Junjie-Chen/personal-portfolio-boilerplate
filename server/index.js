0283164b2940ac95f2e05f9dd125f5e03046f843
Junjie Chen <junjie.chen18@gmail.com>
2017-11-16 15:08:46 -0500

    change the access permissions to files and directories and extract port into a variable

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const port = process.env.PORT || 8080;
 
-app.listen(8080, () => {
-  console.log('Server is listening on port 8080!');
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}!`);
 });
